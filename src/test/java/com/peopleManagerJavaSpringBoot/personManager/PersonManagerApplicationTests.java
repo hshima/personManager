61121928f109d5757877b5abfba4998138a8d742
Henrique Shimada <shimada.henrique@gmail.com>
2020-08-06 21:00:55 -0300

    Initial commit

---
+++ src/test/java/com/peopleManagerJavaSpringBoot/personManager/PersonManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peopleManagerJavaSpringBoot.personManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
