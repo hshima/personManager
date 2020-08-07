f13085e1ca27b924c13757aeec10943602c21537
Henrique Shimada <shimada.henrique@gmail.com>
2020-08-06 23:31:59 -0300

    Adition of entity managed annotations

--- src/main/java/com/peopleManagerJavaSpringBoot/personManager/entity/Phone.java
+++ src/main/java/com/peopleManagerJavaSpringBoot/personManager/entity/Phone.java
@@ -6,12 +6,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+import javax.persistence.*;
+
+@Data // Inserts getters and setters
+@Builder // gives project patterns
+@AllArgsConstructor // Adds Constructors
+@NoArgsConstructor // Adds Constructors
 public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PhoneType type;
+
+    @Column(nullable = false)
     private String number;
 }
